88e307e9b855bbee14d447269d86d9ebdb0851d7
Rajesh9825 <rajuu9825@gmail.com>
2025-04-13 20:59:48 +0530

    dvc added

--- template.py
+++ template.py
@@ -23,7 +23,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
-    "dvc.yaml"
+    "dvc.yaml",
     "app.py",
     "main.py",
     "Dockerfile",
